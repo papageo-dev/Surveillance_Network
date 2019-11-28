6e465a0cea6730aa2c1a45b3524ea94638784851
papageo-dev <40474655+papageo-dev@users.noreply.github.com>
2019-11-28 04:10:06 +0200

    Create method addNumber()
    Add a phone number used by suspect to the ArrayList: "phoneNumbers"

--- SurveillanceNet/src/Suspect.java
+++ SurveillanceNet/src/Suspect.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Suspect {
 	
@@ -6,6 +7,9 @@ public class Suspect {
 	private String codeName; //Suspect's code name
 	private String originCountry; //Suspect's country of origin
 	private String actionCity; //Suspect's city of action
+	
+	//Create an ArrayList object, that will contains phone numbers used by suspect
+	private ArrayList<String> telNumbers = new ArrayList<String>();
 
 	//Suspect's constructor, with arguments
 	public Suspect(String name, String codeName, String originCountry, String actionCity) {
