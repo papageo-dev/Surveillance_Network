2245f62e88c7bff440957133751aafcc5b9624ad
papageo-dev <40474655+papageo-dev@users.noreply.github.com>
2019-11-28 04:03:27 +0200

    Create a Suspect's constructor with fields and initialize them

--- SurveillanceNet/src/Suspect.java
+++ SurveillanceNet/src/Suspect.java
@@ -1,4 +1,21 @@
 
 public class Suspect {
+	
+	//Suspect's fields
+	private String name; //Suspect's name
+	private String codeName; //Suspect's code name
+	private String originCountry; //Suspect's country of origin
+	private String actionCity; //Suspect's city of action
+
+	//Suspect's constructor, with arguments
+	public Suspect(String name, String codeName, String originCountry, String actionCity) {
+		
+		this.name = name; //Initialize suspect's name
+		this.codeName = codeName; //Initialize suspect's code name
+		this.originCountry = originCountry; //Initialize suspect's country of origin
+		this.actionCity = actionCity; //Initialize suspect's city of action
+	}
+	
+	
 
 }
